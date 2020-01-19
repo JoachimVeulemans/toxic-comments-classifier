6ac28cf32e1f1cea6bddf23c03497f4248005692
Joachim Veulemans <joachim.veulemans@outlook.be>
2020-01-19 09:13:43 +0100

    Added Frontend

---
+++ frontend/src/app/app.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        ChartsModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+      providers: [
+        HttpClient, HttpHandler
+      ]
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+});
